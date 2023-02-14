2162eb837f60ff78c0bb4fa7ff5318027f5c9fd8
Nathan Chon <nchon99@gmail.com>
2023-02-14 00:27:47 +0000

    Submit Event Listener #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-var photoUrl = document.querySelector('#photo-url');
-var previewImage = document.querySelector('.preview-image');
-photoUrl.addEventListener('input', function (event) {
-  previewImage.setAttribute('src', event.target.value);
+var $photoUrl = document.querySelector('#photo-url');
+var $previewImage = document.querySelector('.preview-image');
+
+$photoUrl.addEventListener('input', function (event) {
+  $previewImage.setAttribute('src', event.target.value);
+});
+
+var $journalForm = document.querySelector('.code-journal-form');
+$journalForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var info = {};
+  info.name = $journalForm.elements.name.value;
+  info.url = $journalForm.elements.url.value;
+  info.message = $journalForm.elements.message.value;
+
+  $journalForm.reset();
+  $previewImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  info.entryId = data.nextEntryId;
+  data.nextEntryId++;
 });
